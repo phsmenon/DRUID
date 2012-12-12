a46780c501ea88a9e6b147324573076941ce2e7c
phsmenon <phsmenon@gmail.com>
2012-12-11 21:38:16 -0500

    Partial implementation of a type family for controls

--- Druid/Controls.hs
+++ Druid/Controls.hs
@@ -1,3 +1,5 @@
+{-# LANGUAGE TypeFamilies #-}
+
 module Druid.Controls where
 
 import qualified Graphics.UI.WX as WX  hiding ((:=))
@@ -15,10 +17,14 @@ data Label = Label Integer
 
 ---------------------------------------------------------------
 
-class Control w where
+class Widget w where
+  type Delegate w :: *
+  type Property w :: *
   getId :: w -> Integer
+  getDelegate :: w -> Druid(Delegate w)
+  --setProperty w -> Property -> Druid ()
 
-class (Control w) => CommandEventSource w where
+class (Widget w) => CommandEventSource w where
   registerCommandListener :: w -> (UIEvent -> IO ()) -> Druid ()
 
 ---------------------------------------------------------------
@@ -45,6 +51,11 @@ setWidgetProperty id lookup properties = do
   w <- lookup id
   liftIO $ WX.set w properties
 
+{-setProp :: Widget w => w -> [Property w] -> Druid ()-}
+{-setProp widget props = do-}
+  {-wxObj <- getWXWidget $ getId widget-}
+  {-liftIO $ WX.set wxObj props-}
+
 -------------------------------------------------------------------------
 
 createFrame :: String -> Druid Frame
@@ -61,8 +72,12 @@ getWXFrame id = do
 setFrameProperty :: Frame -> [Prop (WX.Frame ())] -> Druid ()
 setFrameProperty (Frame id) props = addUpdateOp $ setWidgetProperty id getWXFrame props
 
-instance Control Frame where
+instance Widget Frame where
+  type Delegate Frame = WX.Frame ()
+  type Property Frame = Prop (WX.Frame ())
   getId (Frame id) = id
+  getDelegate (Frame id) = getWXWidget id >>= \(WXFrame w) -> return w
+  -- setProperty w property = addUpdateOp $ setWidget
 
 ---------------------------------------------------------------  
 
@@ -80,7 +95,10 @@ getWXLabel id = do
 setLabelProperty :: Label -> [Prop (WX.StaticText ())] -> Druid ()
 setLabelProperty (Label id) props = addUpdateOp $ setWidgetProperty id getWXLabel props
   
-instance Control Label where
+instance Widget Label where
+  type Delegate Label = WX.StaticText ()
+  type Property Label = Prop (WX.StaticText ())
+  getDelegate (Label id) = getWXWidget id >>= \(WXLabel w) -> return w
   getId (Label id) = id
 
 ---------------------------------------------------------------  
@@ -100,8 +118,11 @@ getWXButton id = do
 setButtonProperty :: Button -> [Prop (WX.Button ())] -> Druid ()
 setButtonProperty (Button id) props = addUpdateOp $ setWidgetProperty id getWXButton props
 
-instance Control Button where
+instance Widget Button where
+  type Delegate Button = WX.Button ()
+  type Property Button = Prop (WX.Button ())
   getId (Button id) = id
+  getDelegate (Button id) = getWXWidget id >>= \(WXButton w) -> return w
 
 instance CommandEventSource Button where
   registerCommandListener (Button id) handler = deferRegisterEventHandler id getWXButton WX.command (handler $ Command id)
--- Main.hs
+++ Main.hs
@@ -1,12 +1,22 @@
 module Main where
 
 import Druid.DruidMonad
-import Druid.Engine
+import qualified Druid.Engine as E
 import Druid.ReactiveUI
 import Druid.Controls
 
 import qualified Graphics.UI.WX as WX  hiding ((:=))
 import Graphics.UI.WX(Prop((:=)))
+
+import Control.Monad
+
+
+(-=>) = liftM2 (E.-=>)
+
+lift1 = liftM2 E.lift1
+
+accum = liftM2 E.accum
+
 
 initialGui :: Druid(Behavior (Druid ()))
 initialGui = do
@@ -14,12 +24,19 @@ initialGui = do
   l <- createLabel f "Text"
   b <- createButton f "Press Me"
   setButtonProperty b [WX.position := WX.point 50 50]
-  fn l b 
+  fn f l b 
   where
-    fn :: Label -> Button -> Druid(Behavior (Druid ()))
-    fn l b = do event <- onCommand b
-                let beh = accum "" (event -=> ("a" ++))
-                return $ lift1 (\val -> propSet l val) beh
+    fn :: Frame -> Label -> Button -> Druid(Behavior (Druid ()))
+    fn f l b = do beh <- accum (return 0) (onCommand b -=> return (10 +))
+                  lift1 (return (\val -> propSet l (show val))) (return beh)
+    {-fn f l b = do event <- onCommand b-}
+                  {-let beh = accum (0::Integer) (event -=> (25 +))-}
+                {--- return $ lift1 (\val -> propSet l val) beh-}
+                  {-return $ lift1 (\val -> newLabel f val val (show val)) beh-}
+    newLabel :: Frame -> Integer -> Integer -> String -> Druid ()
+    newLabel f x y txt = do 
+      l <- createLabel f txt
+      setLabelProperty l [WX.position := WX.point (fromIntegral x) (fromIntegral y)]
     propSet :: Label -> String -> Druid ()
     propSet l str = setLabelProperty l [WX.text := str]
   
@@ -29,4 +46,4 @@ main = do
   --let initialBehavior = lift1 (setText l) $ accum "0" (onCommand b ==> (\_ -> strInc))
   --start $ startEngine (Just initialGui) (hold never)
   -- putStrLn "In Main"
-  WX.start $ startEngine initialGui
+  WX.start $ E.startEngine initialGui

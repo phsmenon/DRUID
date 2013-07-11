2bf3fedb520b9510a926b429427c1b0973261170
phsmenon <phsmenon@gmail.com>
2013-07-11 11:23:25 -0400

    First cut implementation of an actual graphical component

--- Druid/WXExtensions.hs
+++ Druid/WXExtensions.hs
@@ -6,6 +6,8 @@ module Druid.WXExtensions where
 {-import Graphics.UI.WX(Prop((:=)), Attr(..))-}
 import Graphics.UI.WX
 
+import Control.Monad
+import Control.Applicative
 import Control.Monad.IO.Class
 import Data.IORef
 
@@ -13,6 +15,9 @@ import Druid.DruidMonad
 
 import Debug.Trace
 
+---------------------------------------------------------------------------------
+-- Classes for graphics components and containers
+---------------------------------------------------------------------------------
 
 class GraphicsContainer w where
   addGraphics :: GraphicsComponent g => w -> g -> IO ()
@@ -30,26 +35,78 @@ data AnyGraphicsContainer = forall w. GraphicsContainer w => AnyGraphicsContaine
 data AnyGraphicsComponent = forall w. GraphicsComponent w => AnyGraphicsComponent w
 
 ---------------------------------------------------------------------------------
+-- "Abstract" class for simple graphics
+---------------------------------------------------------------------------------
 
-data DimensionData = DimensionData { area :: Rect }
+data SimpleGraphicsData = SimpleGraphicsData 
+  { sgdContainer :: Maybe AnyGraphicsContainer, sgdBounds :: Rect, sgdBestSize :: Size }
 
 class SimpleGraphics g where
-  getParentData :: g -> IORef (Maybe AnyGraphicsContainer)
-  getDimensionData :: g -> IORef DimensionData
+  getSimpleGraphicsData :: g -> IORef SimpleGraphicsData
   draw :: g -> DC () -> IO ()
+
+fetchSimpleGraphicsData :: SimpleGraphics g => g -> (SimpleGraphicsData -> a) -> IO a
+fetchSimpleGraphicsData g processFn = processFn <$> readIORef (getSimpleGraphicsData g)
+
+updateSimpleGraphicsData :: SimpleGraphics g => g -> (SimpleGraphicsData -> SimpleGraphicsData) -> IO ()
+updateSimpleGraphicsData g processFn = modifyIORef (getSimpleGraphicsData g) processFn
+
 
 newtype SimpleGraphicsWrapper a = SimpleGraphicsWrapper { unwrapSimpleGraphics :: a }
 
+instance SimpleGraphics g => SimpleGraphics (SimpleGraphicsWrapper g) where
+  getSimpleGraphicsData g = getSimpleGraphicsData $ unwrapSimpleGraphics g
+  draw g dc = draw (unwrapSimpleGraphics g) dc
+
+
 instance SimpleGraphics g => GraphicsComponent (SimpleGraphicsWrapper g) where
-  onAttachToContainer g w = writeIORef (getParentData $ unwrapSimpleGraphics g) (Just $ AnyGraphicsContainer w)
-  onDetachFromContainer g = writeIORef (getParentData $ unwrapSimpleGraphics g) (Nothing)
-  onDraw g dc = draw (unwrapSimpleGraphics g) dc
+  onAttachToContainer g w = updateSimpleGraphicsData g (\r -> r { sgdContainer = Just $ AnyGraphicsContainer w} )
+  onDetachFromContainer g = updateSimpleGraphicsData g (\r -> r { sgdContainer = Nothing} )
+  onDraw g dc = draw g dc
 
-{-instance SimpleGrahics g => Dimensions (SimpleGraphicsWrapper g) where-}
-  {-outerSize :: -}
 
+instance SimpleGraphics g => Dimensions (SimpleGraphicsWrapper g) where
+  outerSize = newAttr "outerSize" readSize writeSize where
+    readSize g = fetchSimpleGraphicsData g (rectSize . sgdBounds)
+    writeSize g sz = updateSimpleGraphicsData g (\r -> r { sgdBounds = resizeRect (sgdBounds r) sz })
+  position  = newAttr "position" readPos writePos where
+    readPos g = fetchSimpleGraphicsData g (rectTopLeft . sgdBounds)
+    writePos g pt = updateSimpleGraphicsData g (\r -> r { sgdBounds = repositionRect (sgdBounds r) pt})
+  area = newAttr "area" readArea writeArea where
+    readArea g = fetchSimpleGraphicsData g sgdBounds
+    writeArea g ar = updateSimpleGraphicsData g (\r -> r { sgdBounds = ar})
+  bestSize = readAttr "bestSize" (flip fetchSimpleGraphicsData $ sgdBestSize)
+  clientSize = newAttr "clientSize" readSize writeSize where
+    readSize g = fetchSimpleGraphicsData g (rectSize . sgdBounds)
+    writeSize g sz = updateSimpleGraphicsData g (\r -> r { sgdBounds = resizeRect (sgdBounds r) sz })
+  virtualSize = newAttr "virtualSize" readSize writeSize where
+    readSize g = fetchSimpleGraphicsData g (rectSize . sgdBounds)
+    writeSize g sz = updateSimpleGraphicsData g (\r -> r { sgdBounds = resizeRect (sgdBounds r) sz })
+
+
+----------------------------------------------------------------------------------
+-- Basic Graphics components
+----------------------------------------------------------------------------------
+
+data Rectangle_ = Rectangle (IORef SimpleGraphicsData)
+
+instance SimpleGraphics Rectangle_ where
+  getSimpleGraphicsData (Rectangle sgd) = sgd
+  draw g dc = fetchSimpleGraphicsData g sgdBounds >>= (\bounds -> drawRect dc bounds [])
+
+type Rectangle = SimpleGraphicsWrapper Rectangle_  
+
+----------------------------------------------------------------------------------
+-- Basic Graphics Containers
 ----------------------------------------------------------------------------------
 
 
+----------------------------------------------------------------------------------
+-- General WX helpers
+----------------------------------------------------------------------------------
 
+resizeRect :: Rect -> Size -> Rect
+resizeRect rc sz = rect (rectTopLeft rc) sz
 
+repositionRect :: Rect -> Point -> Rect
+repositionRect rc pt = rect pt (rectSize rc)

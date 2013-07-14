ee551290cc202458eda16e3fb2c9e958e21a3182
phsmenon <phsmenon@gmail.com>
2013-07-14 12:53:19 -0400

    Initial implementation of a Rectangle graphics component and a Frame graphics container

--- Druid/WXExtensions.hs
+++ Druid/WXExtensions.hs
@@ -10,6 +10,11 @@ import Control.Monad
 import Control.Applicative
 import Control.Monad.IO.Class
 import Data.IORef
+import Data.List
+import Data.Unique
+import System.IO.Unsafe
+
+import qualified Data.HashTable.IO as H
 
 import Druid.DruidMonad
 
@@ -25,6 +30,7 @@ class GraphicsContainer w where
   requestRepaint :: w -> IO ()
 
 class GraphicsComponent g where
+  getComponentId :: g -> Integer
   onAttachToContainer :: GraphicsContainer w => g -> w -> IO ()
   onDetachFromContainer :: g -> IO ()
   onDraw :: g -> DC () -> IO ()
@@ -33,6 +39,13 @@ class GraphicsComponent g where
 data AnyGraphicsContainer = forall w. GraphicsContainer w => AnyGraphicsContainer w
 
 data AnyGraphicsComponent = forall w. GraphicsComponent w => AnyGraphicsComponent w
+
+instance Eq AnyGraphicsComponent where
+  (AnyGraphicsComponent w1) == (AnyGraphicsComponent w2) = (getComponentId w1) == (getComponentId w2)
+
+instance Ord AnyGraphicsComponent where
+  compare (AnyGraphicsComponent w1) (AnyGraphicsComponent w2) = 
+    compare (getComponentId w1) (getComponentId w2)
 
 ---------------------------------------------------------------------------------
 -- "Abstract" class for simple graphics
@@ -43,6 +56,7 @@ data SimpleGraphicsData = SimpleGraphicsData
 
 class SimpleGraphics g where
   getSimpleGraphicsData :: g -> IORef SimpleGraphicsData
+  getId :: g -> Integer
   draw :: g -> DC () -> IO ()
 
 fetchSimpleGraphicsData :: SimpleGraphics g => g -> (SimpleGraphicsData -> a) -> IO a
@@ -57,12 +71,14 @@ newtype SimpleGraphicsWrapper a = SimpleGraphicsWrapper { unwrapSimpleGraphics :
 instance SimpleGraphics g => SimpleGraphics (SimpleGraphicsWrapper g) where
   getSimpleGraphicsData g = getSimpleGraphicsData $ unwrapSimpleGraphics g
   draw g dc = draw (unwrapSimpleGraphics g) dc
+  getId g = getId (unwrapSimpleGraphics g)
 
 
 instance SimpleGraphics g => GraphicsComponent (SimpleGraphicsWrapper g) where
   onAttachToContainer g w = updateSimpleGraphicsData g (\r -> r { sgdContainer = Just $ AnyGraphicsContainer w} )
   onDetachFromContainer g = updateSimpleGraphicsData g (\r -> r { sgdContainer = Nothing} )
   onDraw g dc = draw g dc
+  getComponentId g = getId g
 
 
 instance SimpleGraphics g => Dimensions (SimpleGraphicsWrapper g) where
@@ -88,18 +104,60 @@ instance SimpleGraphics g => Dimensions (SimpleGraphicsWrapper g) where
 -- Basic Graphics components
 ----------------------------------------------------------------------------------
 
-data Rectangle_ = Rectangle (IORef SimpleGraphicsData)
+getNextComponentId :: IO Integer
+getNextComponentId = newUnique >>= \u -> return . fromIntegral $ hashUnique u
+
+
+data Rectangle_ = Rectangle (Integer, IORef SimpleGraphicsData)
 
 instance SimpleGraphics Rectangle_ where
-  getSimpleGraphicsData (Rectangle sgd) = sgd
+  getSimpleGraphicsData (Rectangle (_, sgd)) = sgd
   draw g dc = fetchSimpleGraphicsData g sgdBounds >>= (\bounds -> drawRect dc bounds [])
+  getId (Rectangle (id, _))= id
 
 type Rectangle = SimpleGraphicsWrapper Rectangle_  
+
+createRectangle :: GraphicsContainer w => w -> [Prop Rectangle] -> IO Rectangle
+createRectangle w props = do
+  id <- getNextComponentId
+  sgdRef <- newIORef $ makeInitialRecord w
+  makeRectangle id sgdRef
+  where
+    makeInitialRecord w = SimpleGraphicsData {
+      sgdContainer = Just $ AnyGraphicsContainer w, 
+      sgdBounds = rectFromSize $ sz 100 100, 
+      sgdBestSize = sz 100 100}
+    makeRectangle id sgdRef = do
+      let rectangle = SimpleGraphicsWrapper $ Rectangle (id, sgdRef)
+      set rectangle props
+      return rectangle
 
 ----------------------------------------------------------------------------------
 -- Basic Graphics Containers
 ----------------------------------------------------------------------------------
 
+type GraphicsComponentCollection = H.BasicHashTable Int [AnyGraphicsComponent]
+
+graphicsComponentData :: GraphicsComponentCollection
+graphicsComponentData = unsafePerformIO $ H.new
+
+addGraphicsComponent :: GraphicsComponentCollection -> Int -> AnyGraphicsComponent -> IO ()
+addGraphicsComponent collection id g = do
+  maybeList <- H.lookup collection id
+  H.insert collection id $ maybe [g] (g :) maybeList
+
+removeGraphicsComponent :: GraphicsComponentCollection -> Int -> AnyGraphicsComponent -> IO ()
+removeGraphicsComponent collection id g = do
+  maybeList <- H.lookup collection id
+  H.insert collection id $ maybe [] (delete g) maybeList
+
+
+
+instance GraphicsContainer (Frame a) where
+  addGraphics fr g = get fr identity >>= \id -> addGraphicsComponent graphicsComponentData id (AnyGraphicsComponent g)
+  removeGraphics fr g = get fr identity >>= \id -> removeGraphicsComponent graphicsComponentData id (AnyGraphicsComponent g)
+  requestRepaint fr = repaint fr
+  
 
 ----------------------------------------------------------------------------------
 -- General WX helpers
